35988b419ab5b157ee9ba81882292b4c760ed414
oleksandr.luchkevych.ri.2022@lpnu.ua <oleksandr.luchkevych.ri.2022@lpnu.ua>
2023-05-04 20:31:36 +0300

    Add project files.

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_12.10/Lab_12.10.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Node* NoDe = new Node;
+			NoDe->data = 10;
+			Node* node = createNode(10);
+
+			Assert::AreEqual(node, NoDe);
+		}
+	};
+}
